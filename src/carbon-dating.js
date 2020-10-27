7cd01fdf90a8ed61ba83cb0e1c6867a22d4ad31d
yarnax <feniks_63@mail.ru>
2020-10-27 17:48:08 +0300

    feat: complete tasks - count cats, carbone dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,17 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sample) {
+  console.log("sample age = " + sample + " -- " + typeof sample)
+  if (typeof sample !== "string") { return false }
+  sample = parseInt(sample);
+  if (sample !== "number") { return false }
+  console.log("parsed sample = " + sample)
+  
+  if (sample <= 0 || sample > 16) {
+    return false;
+  }
+  let k = 0.623 / HALF_LIFE_PERIOD;
+  let age = Math.ceil( (Math.log(MODERN_ACTIVITY / sample)) / k)
+  return age;
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,13 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let catCount=0;
+  matrix.forEach(subarray => {
+    subarray.forEach(element => {
+      if (element === "^^") {
+        catCount ++;
+      }
+    });
+  });
+  return catCount;
 };
