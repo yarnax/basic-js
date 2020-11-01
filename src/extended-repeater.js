3f9d142d5bddc2696429c562549c9e2ae668b21f
yarnax <feniks_63@mail.ru>
2020-11-01 13:04:58 +0300

    feat: done "extended-repeater" task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,43 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+  let answerRepeat = "";
+  let repeatTimes = options.repeatTimes;
+  let additionRepeatTimes = options.additionRepeatTimes;
+  let addition = options.addition;
+  let separator = options.separator;
+  let additionSeparator = options.additionSeparator;
+
+  //str = str.toString();
+  //addition = addition.toString();
+  if ((additionRepeatTimes === undefined || additionRepeatTimes === null) && (repeatTimes === undefined || repeatTimes === null)) {return str + addition}
+  if (separator === undefined || separator === null) { separator = "+" }
+  if (additionSeparator === undefined || additionSeparator === null) { additionSeparator = "|" }
+  if (repeatTimes === undefined || repeatTimes === null) {repeatTimes = 0}
+  if (additionRepeatTimes === undefined || additionRepeatTimes === null) {additionRepeatTimes = 0}
+  
+
+  console.log("start data");
+  console.log(str);
+  console.log(options);
+
+  for (let repT = 0; repT < repeatTimes; repT++) {
+    answerRepeat += str;
+    for (let adrepT = 0; adrepT < additionRepeatTimes; adrepT++) {
+      answerRepeat += addition;
+      if (adrepT < additionRepeatTimes - 1) {
+        answerRepeat += additionSeparator;
+      }  
+    }
+
+    if (repT < repeatTimes - 1) {
+      answerRepeat += separator;
+    } 
+    
+    
+  }
+  console.log(answerRepeat);
+  return answerRepeat;
+
 };
   
